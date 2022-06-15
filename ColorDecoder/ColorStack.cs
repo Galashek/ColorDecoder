ff90240af1d4c9ac2734d41399a4ed40a1b0048f
Galishev Nikita <shpegala@mail.ru>
2022-06-15 13:45:39 +0500

    Добавьте файлы проекта.

---
+++ ColorDecoder/ColorStack.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ColorDecoder
+{
+    public class ColorStack
+    {
+        public static readonly SolidColorBrush[] AllColors = 
+        { 
+            Brushes.Crimson, 
+            Brushes.Green, 
+            Brushes.RoyalBlue, 
+            Brushes.DarkViolet, 
+            Brushes.Yellow, 
+            Brushes.Orange 
+        };
+        public static readonly SolidColorBrush EmptyColor = Brushes.DarkGray;
+        public Grid Stack { get; }
+        public SolidColorBrush[] Colors => buttons.Select(b => b.Fill).Cast<SolidColorBrush>().ToArray();
+        private readonly Shape[] buttons;
+
+        public ColorStack()
+        {
+            Stack = new Grid
+            {
+                Width = 200,
+                Height = 60
+            };
+            buttons = new Shape[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Stack.ColumnDefinitions.Add(new ColumnDefinition());
+                var colorButton = new Ellipse()
+                {
+                    Width = 40,
+                    Height = 40,
+                    Fill = EmptyColor,
+                    Stroke = Brushes.DarkSlateGray
+                };
+                var j = i;
+                colorButton.MouseUp += (s, e) => ChangeColor(j);
+                colorButton.Tag = -1;
+                buttons[i] = colorButton;
+                Grid.SetColumn(colorButton, i);
+                Stack.Children.Add(colorButton);
+            }
+        }
+
+        public void SetColors(SolidColorBrush[] colors)
+        {
+            if (colors.Length < 4) return;
+            for (int i = 0; i < 4; i++)
+            {
+                buttons[i].Fill = colors[i];
+            }
+        }
+
+        private void ChangeColor(int index)
+        {
+            var button = buttons[index];
+            var curColorIndex = (int)button.Tag;
+            int newColorIndex;
+            if (curColorIndex == -1)
+                newColorIndex = 0;
+            else newColorIndex = (curColorIndex + 1) % 6;
+            button.Fill = AllColors[newColorIndex];
+            button.Tag = newColorIndex;
+        }
+
+        public void Disable()
+        {
+            foreach (var button in buttons)
+            {
+                button.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ ColorDecoder/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ColorDecoder
+{
+    public partial class MainWindow
+    {
+        private ColorStack answerStack;
+        private ColorStack tryStack;
+        private SolidColorBrush[] colorsToGuess;
+        private int tryCount;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartGame(null, null);
+        }
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+            tryCount = 0;
+            GameField.Children.Clear();
+            if (GamePanel.Children.Count > 2)
+                GamePanel.Children.RemoveAt(2);
+            CheckButton.IsEnabled = true;
+
+            colorsToGuess = GetRandomColors();
+            answerStack = new ColorStack();
+            answerStack.Disable();
+            GamePanel.Children.Add(answerStack.Stack);
+            // answerStack.SetColors(colorsToGuess);
+            
+            NewTry();
+        }
+        private void NewTry()
+        {
+            tryStack?.Disable();
+            var stack = new ColorStack();
+            Grid.SetRow(stack.Stack, tryCount);
+            Grid.SetColumn(stack.Stack, 1);
+            GameField.Children.Add(stack.Stack);
+            tryStack = stack;
+        }
+        private void EndGame()
+        {
+            CheckButton.IsEnabled = false;
+            answerStack.SetColors(colorsToGuess);
+            // MessageBox.Show("End");
+        }
+        private void Check(object sender, RoutedEventArgs e)
+        {
+            var curStackColors = tryStack.Colors;
+            if (curStackColors.Any(c => c == ColorStack.EmptyColor)) return;
+            var colorAndPositionMatch = 0;
+            var onlyColorMatch = curStackColors.Distinct().Count(color => colorsToGuess.Contains(color));
+            for (int i = 0; i < 4; i++)
+            {
+                if (curStackColors[i] == colorsToGuess[i])
+                {
+                    colorAndPositionMatch++;
+                    onlyColorMatch--;
+                }
+            }
+            var matchStack = new MatchStack(onlyColorMatch, colorAndPositionMatch);
+            Grid.SetRow(matchStack.Stack, tryCount);
+            Grid.SetColumn(matchStack.Stack, 0);
+            GameField.Children.Add(matchStack.Stack);
+            tryCount++;
+            if (colorAndPositionMatch == 4 || tryCount == 6) EndGame();
+            else NewTry();
+        }
+        private SolidColorBrush[] GetRandomColors()
+        {
+            var colors = new SolidColorBrush[4];
+            var rnd = new Random();
+            var colorList = new List<SolidColorBrush>(ColorStack.AllColors);
+            for (int i = 0; i < 4; i++)
+            {
+                var index = rnd.Next(0, colorList.Count);
+                colors[i] = colorList[index];
+                colorList.RemoveAt(index);
+            }
+            return colors;
+        }
+    }
+}
